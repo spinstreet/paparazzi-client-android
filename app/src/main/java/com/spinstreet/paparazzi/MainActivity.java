9416d8460751b29b6bb79f3d0bb03813b05daeb2
Dominic Schaff <dominic.schaff@gmail.com>
2017-01-21 18:03:09 +0200

    Profile and menu

--- app/src/main/java/com/spinstreet/paparazzi/MainActivity.java
+++ app/src/main/java/com/spinstreet/paparazzi/MainActivity.java
@@ -1,10 +1,14 @@
 package com.spinstreet.paparazzi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,4 +30,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //respond to menu item selection
+
+        switch (item.getItemId()) {
+            case R.id.set_profile:
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
