7d98500f517aada782614cdeb00c23f159f7922c
Dominic Schaff <dominic.schaff@gmail.com>
2017-01-21 17:47:17 +0200

    Get error messages

--- app/src/main/java/com/spinstreet/paparazzi/LoginActivity.java
+++ app/src/main/java/com/spinstreet/paparazzi/LoginActivity.java
@@ -2,8 +2,10 @@ package com.spinstreet.paparazzi;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -107,8 +109,28 @@ public class LoginActivity extends AppCompatActivity {
                     .setCallback(new FutureCallback<Response<JsonObject>>() {
                         @Override
                         public void onCompleted(Exception e, Response<JsonObject> result) {
-
                             if (e != null) {
+                                showProgress(false);
+                                new AlertDialog.Builder(getApplicationContext())
+                                        .setMessage(e.getMessage())
+                                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // User clicked OK button
+                                            }
+                                        })
+                                        .create().show();
+                                return;
+                            }
+                            if (result.getHeaders().code() != 200) {
+                                showProgress(false);
+                                new AlertDialog.Builder(getApplicationContext())
+                                        .setMessage(result.getHeaders().message())
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // User clicked OK button
+                                    }
+                                })
+                                .create().show();
                                 return;
                             }
                             Session.username = email;
